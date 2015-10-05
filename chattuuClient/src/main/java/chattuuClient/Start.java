2c4b62c5fe9f4db35635b5fcfe0978111ea2ff6b
Jeimy Rojas <Gregory Mullaney@10.111.0.175>
2015-10-05 18:14:27 -0300

    Veremos =(

--- chattuuClient/src/main/java/chattuuClient/Start.java
+++ chattuuClient/src/main/java/chattuuClient/Start.java
@@ -5,6 +5,9 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/**
+		 * Prueba, comentar debajo para probar
+		 * */
 	}
 
 }
--- chattuuServer/src/main/java/chattuuServer/Start.java
+++ chattuuServer/src/main/java/chattuuServer/Start.java
@@ -5,6 +5,9 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO
 
+		/**
+		 * Prueba, comentar debajo para probar
+		 * */
 	}
 
 }
