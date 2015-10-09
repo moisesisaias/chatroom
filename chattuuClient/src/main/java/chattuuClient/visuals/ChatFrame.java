24c7da48002b31bf7795b640f9c2b3302a359205
Jeimy <rojaslopez.jeimy@gmail.com>
2015-10-09 07:19:43 -0400

    Visual del cliente lista

--- chattuuClient/src/main/java/chattuuClient/visuals/ChatFrame.java
+++ chattuuClient/src/main/java/chattuuClient/visuals/ChatFrame.java
@@ -3,10 +3,18 @@ package chattuuClient.visuals;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.JTextArea;
+import java.awt.Dimension;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
 
 public class ChatFrame {
 
-	private JFrame frame;
+	private JFrame frmChattuu;
 
 	/**
 	 * Launch the application.
@@ -16,7 +24,7 @@ public class ChatFrame {
 			public void run() {
 				try {
 					ChatFrame window = new ChatFrame();
-					window.frame.setVisible(true);
+					window.frmChattuu.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +43,56 @@ public class ChatFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmChattuu = new JFrame();
+		frmChattuu.setTitle("Chattuu");
+		frmChattuu.setMinimumSize(new Dimension(500, 350));
+		frmChattuu.setBounds(100, 100, 500, 433);
+		frmChattuu.setBounds(100, 100, 500, 300);
+		frmChattuu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmChattuu.getContentPane().setLayout(new MigLayout("", "[110px:150px:450px,grow][200px:250.00,grow][110px:110.00]", "[grow][70px:75px][70px:75px]"));
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane_1.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		frmChattuu.getContentPane().add(scrollPane_1, "cell 0 0 1 3,grow");
+		
+		JTextArea txtrLog = new JTextArea();
+		txtrLog.setEditable(false);
+		txtrLog.setLineWrap(true);
+		txtrLog.setWrapStyleWord(true);
+		txtrLog.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		scrollPane_1.setViewportView(txtrLog);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		frmChattuu.getContentPane().add(scrollPane, "cell 1 0 2 1,grow");
+		
+		JTextArea txtrMsglog = new JTextArea();
+		txtrMsglog.setEditable(false);
+		txtrMsglog.setLineWrap(true);
+		txtrMsglog.setWrapStyleWord(true);
+		txtrMsglog.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		scrollPane.setViewportView(txtrMsglog);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		scrollPane_2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		frmChattuu.getContentPane().add(scrollPane_2, "cell 1 1 1 2,grow");
+		
+		JTextArea txtrWritemsg = new JTextArea();
+		txtrWritemsg.setLineWrap(true);
+		txtrWritemsg.setWrapStyleWord(true);
+		txtrWritemsg.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		scrollPane_2.setViewportView(txtrWritemsg);
+		
+		JButton btnSend = new JButton("Enviar");
+		btnSend.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		frmChattuu.getContentPane().add(btnSend, "cell 2 1,grow");
+		
+		JButton btnDisconnect = new JButton("Desconectar");
+		btnDisconnect.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		frmChattuu.getContentPane().add(btnDisconnect, "cell 2 2,grow");
 	}
 
 }
--- chattuuClient/src/main/java/chattuuClient/visuals/MainFrame.java
+++ chattuuClient/src/main/java/chattuuClient/visuals/MainFrame.java
@@ -3,10 +3,22 @@ package chattuuClient.visuals;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EtchedBorder;
 
 public class MainFrame {
 
-	private JFrame frame;
+	private JFrame frmChattuuLog;
+	private JTextField txtIp;
+	private JTextField txtPort;
 
 	/**
 	 * Launch the application.
@@ -16,7 +28,7 @@ public class MainFrame {
 			public void run() {
 				try {
 					MainFrame window = new MainFrame();
-					window.frame.setVisible(true);
+					window.frmChattuuLog.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +47,48 @@ public class MainFrame {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmChattuuLog = new JFrame();
+		frmChattuuLog.setTitle("Chattuu Log");
+		frmChattuuLog.setResizable(false);
+		frmChattuuLog.setBounds(100, 100, 500, 270);
+		frmChattuuLog.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmChattuuLog.getContentPane().setLayout(new MigLayout("", "[104.00][161.00][20.00][113.00][63.00]", "[20px][30px][20px][30px][20px][30px][20px][30px][20.00]"));
+		
+		JLabel lblTitle = new JLabel("Bienvenidos a Chattuu");
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 17));
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		frmChattuuLog.getContentPane().add(lblTitle, "cell 1 1 3 1,grow");
+		
+		JLabel lblIp = new JLabel("Ip Servidor");
+		lblIp.setFont(new Font("Tahoma", Font.BOLD, 11));
+		frmChattuuLog.getContentPane().add(lblIp, "cell 0 3,alignx trailing,growy");
+		
+		txtIp = new JTextField();
+		txtIp.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		frmChattuuLog.getContentPane().add(txtIp, "cell 1 3,grow");
+		txtIp.setColumns(10);
+		
+		JButton btnConnect = new JButton("Conectar");
+		btnConnect.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		frmChattuuLog.getContentPane().add(btnConnect, "cell 3 3,grow");
+		
+		JLabel lblPort = new JLabel("Puerto");
+		lblPort.setFont(new Font("Tahoma", Font.BOLD, 11));
+		frmChattuuLog.getContentPane().add(lblPort, "cell 0 5,alignx trailing,growy");
+		
+		txtPort = new JTextField();
+		txtPort.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		frmChattuuLog.getContentPane().add(txtPort, "cell 1 5,grow");
+		txtPort.setColumns(10);
+		
+		JButton btnExit = new JButton("Salir");
+		btnExit.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		frmChattuuLog.getContentPane().add(btnExit, "cell 3 5,grow");
+		
+		JTextArea txtrErrorlog = new JTextArea();
+		txtrErrorlog.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		txtrErrorlog.setEditable(false);
+		frmChattuuLog.getContentPane().add(txtrErrorlog, "cell 1 7 3 1,grow");
 	}
 
 }
